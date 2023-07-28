76a1c98f49cf7599c14149e2e3cb67a05114024c
adrien <adrienjacob1111@gmail.com>
2023-07-28 16:38:58 +0200

    Flex Image Input

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, Pressable, Image } from 'react-native';
 
 export default function App() {
   const [isOn, setIsOn] = useState(true);
+  const [catimg, setCatimg] = useState(require('./assets/yoyo.png'))
 
   const switchOnStyle = {
     backgroundColor: 'red',
@@ -22,16 +23,36 @@ export default function App() {
     setIsOn(false);
   };
 
+  const catChange = () => {
+    if (catimg === require('./assets/yoyo.png')) {
+      setCatimg(require('./assets/icon.png'))
+    } else {
+      setCatimg(require('./assets/yoyo.png'))
+    }
+  }
+
+
+
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Pressable onPress={handleSwitchOnPress}>
-        <Text style={[styles.button, isOn ? switchOnStyle : switchOffStyle]}>Switch On</Text>
+
+      <View style={styles.viewbox}>
+        <Pressable onPress={handleSwitchOnPress}>
+          <Text style={[styles.button, isOn ? switchOnStyle : switchOffStyle]}>Switch On</Text>
+        </Pressable>
+        <Pressable onPress={handleSwitchOffPress}>
+          <Text style={[styles.button, isOn ? switchOffStyle : switchOnStyle]}>Switch Off</Text>
+        </Pressable>
+      </View>
+
+      <Pressable onPress={catChange}>
+        <Image source={catimg} style={styles.imgstyle} />
       </Pressable>
-      <Pressable onPress={handleSwitchOffPress}>
-        <Text style={[styles.button, isOn ? switchOffStyle : switchOnStyle]}>Switch Off</Text>
-      </Pressable>
-    </View>
+
+
+    </SafeAreaView>
   );
 }
 
@@ -49,4 +70,15 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
+  imgstyle: {
+    width: 350,
+    height: 350,
+    resizeMode: 'contain'
+  },
+  viewbox : {
+    flexDirection : 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+  }
+
 });
