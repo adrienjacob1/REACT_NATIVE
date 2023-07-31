64c2f1aab877297a8c41e624d9760f7d334fa86c
adrien <adrienjacob1111@gmail.com>
2023-07-31 16:20:01 +0200

    Liste

--- App.js
+++ App.js
@@ -3,12 +3,44 @@ import { NavigationContainer } from '@react-navigation/native';
 import InfoLevelOne from './components/InfoLevelOne';
 import { InfoLevelTwo } from './components/InfoLevelTwo';
 import Home from './components/Home';
+import { SafeAreaView, FlatList, View, Text } from 'react-native';
 
 
+const DATA = [
+  { id: '1',
+    title : 'Level1'},
+  { id: '2',
+   title : 'Level3'},
+  { id: '3',
+   title : 'Level3'},
+];
 
+const Item = ({title}) => (
+  <View>
+    <Text>{title}</Text>
+  </View>
+)
 
 export default function App() {
-  return (
+
+
+  return(
+    <SafeAreaView>
+    <FlatList
+      data={DATA}
+      renderItem={({item}) => <Item title={item.title}/>}
+      keyExtractor={item => item.id}
+    />
+
+  </SafeAreaView>
+  )
+
+
+
+
+
+
+ /* return (
 
     <NavigationContainer>
       <StackNavigator/>
@@ -16,14 +48,14 @@ export default function App() {
 
 
 
-    /*<View style={styles.container}>
+    <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
 
       <InfoPerso></InfoPerso>
     <Button persoTxt="testestest"></Button>
-    </View>*/
-  );
+    </View>
+  );*/
 }
 
 const Stack = createStackNavigator();
